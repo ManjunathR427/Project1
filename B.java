e077c8a9396b7a65f0c58494d1e940cf9faedfb2
unknown <ManjunathR427>
2020-08-30 23:48:14 +0530

    Fourth Commit

--- B.java
+++ B.java
@@ -1,5 +1,5 @@
 public class B {
 public static void main (String [] args) {
-System.out.println("Baba");
+System.out.println("Baba");// Baba is god
 }
 }
--- F.java
+++ F.java
@@ -1,4 +1,4 @@
-public class D {
+public class F {
 public static void main (String [] args) {
 System.out.println("Baba");
 }
